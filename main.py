e98730c82c904363690484a217879afba4205fe2
Uju04 <129797892+Uju04@users.noreply.github.com>
2023-04-22 18:07:19 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+name = input("enter name:")
+age = input("enter age:")
+if age <= "16" :
+    print("not old enough")
+else:
+    print("welcome to wonderland")
---
+++ velocity.py
@@ -0,0 +1,5 @@
+u = 18
+a = 7
+t = 3
+sum = a*t
+print(u + "sum:")
